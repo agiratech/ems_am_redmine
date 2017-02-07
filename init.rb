f843b70f3657db1ffdadbfd79d3b522fe66f1c9c
agiratech-jagadeesh <jagadeesh@agiratech.com>
2017-02-07 17:09:04 +0530

    changes in configuration

--- init.rb
+++ init.rb
@@ -1,17 +1,17 @@
 Redmine::Plugin.register :ems_am_redmine do
-  name 'Ems Am Redmine Plugin plugin'
-  author 'Author name'
+  name 'Ems Am Redmine Plugin'
+  author 'Agira Technologies'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/agiratech/ems_am_redmine'
+  author_url 'http://www.agiratech.com/'
 end
 
 Rails.application.config.to_prepare do
-	Project.send(:include, ProjectPatch)
-	Issue.send(:include,IssuePatch)
-	ApplicationHelper.send(:include, ApplicationHelperPatch)
-	IssuesController.send(:include,IssuesControllerPatch)
+  Project.send(:include, ProjectPatch)
+  Issue.send(:include,IssuePatch)
+  ApplicationHelper.send(:include, ApplicationHelperPatch)
+  IssuesController.send(:include,IssuesControllerPatch)
   Query.send(:include,QueryPatch)
   IssueQuery.send(:include,IssueQueryPatch)
   ProjectsController.send(:include,ProjectsControllerPatch)
