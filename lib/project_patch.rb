3f5ce0c8ee4baddaf244d40afaf8db1ddbe352b7
agiratech-jagadeesh <jagadeesh@agiratech.com>
2017-02-08 12:57:14 +0530

    Accesssing assigned projects

--- lib/project_patch.rb
+++ lib/project_patch.rb
@@ -1,12 +1,18 @@
 module ProjectPatch
+
 	def self.included(base)
 		base.send(:include, InstanceMethods)
-    base.class_eval do
-    end
+		base.class_eval do
+		end
 	end
+
 	module InstanceMethods
 		def projects_list
-			Project.all
+			if User.current.admin?
+				Project.active
+			else
+				User.last.projects.active && Project.where(is_public:true).active
+			end
 		end
 	end
 end
